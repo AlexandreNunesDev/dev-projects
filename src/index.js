efcfbb11ec7f2ede0a0621edae135ba86800aeb4
AlexandreNunesDev <74550620+AlexandreNunesDev@users.noreply.github.com>
2020-11-16 16:54:20 -0300

    second-commit
    second commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App'
 import { BrowserRouter } from 'react-router-dom';
 import { ToastProvider } from 'react-toast-notifications';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
