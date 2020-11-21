d14dc13b70551b03b365b6d6b08108030a7887fe
AlexandreNunesDev <74550620+AlexandreNunesDev@users.noreply.github.com>
2020-11-21 18:24:39 -0300

    Update ScqApi.js

--- src/Http/ScqApi.js
+++ src/Http/ScqApi.js
@@ -3,7 +3,7 @@ import { getToken } from "../Services/auth"
 
 
 const http = axios.create({
-    baseURL: "http://ec2-18-217-150-107.us-east-2.compute.amazonaws.com:8080"
+    baseURL: "https://ec2-18-217-150-107.us-east-2.compute.amazonaws.com:8080"
     
 })
 
