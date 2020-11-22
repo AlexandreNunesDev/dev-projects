defaa18c30e5574661340ee00344e04002695f7d
AlexandreNunesDev <74550620+AlexandreNunesDev@users.noreply.github.com>
2020-11-22 09:28:23 -0300

    Update ScqApi.js

--- src/Http/ScqApi.js
+++ src/Http/ScqApi.js
@@ -3,7 +3,7 @@ import { getToken } from "../Services/auth"
 
 
 const http = axios.create({
-    baseURL: encodeURI("http://scqapi-env-1.eba-yk7mty23.us-east-2.elasticbeanstalk.com/")
+    baseURL: encodeURI("https://scqapi-env-1.eba-yk7mty23.us-east-2.elasticbeanstalk.com/")
     
 })
 
