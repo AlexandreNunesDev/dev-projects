ce2ca728f1682e566ac657d9ae85480fabaf0d96
AlexandreNunesDev <74550620+AlexandreNunesDev@users.noreply.github.com>
2020-11-23 00:55:58 -0300

    Update ScqApi.js

--- src/Http/ScqApi.js
+++ src/Http/ScqApi.js
@@ -3,7 +3,7 @@ import { getToken } from "../Services/auth"
 
 
 const http = axios.create({
-    baseURL: encodeURI("http://scaapi-env.eba-gtwwaim2.sa-east-1.elasticbeanstalk.com/")
+    baseURL: encodeURI("https://scaapi-env.eba-gtwwaim2.sa-east-1.elasticbeanstalk.com/")
     
 })
 
