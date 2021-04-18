ad8735910d15cd3fa2c18bb00027b91cc29c920b
AlexandreNunesDev <74550620+AlexandreNunesDev@users.noreply.github.com>
2021-04-18 09:05:29 -0300

    Update AnaliseChart.js
    reference line label insert and scale adjust

--- src/Components/AnaliseChart.js
+++ src/Components/AnaliseChart.js
@@ -39,7 +39,8 @@ const AnaliseChart = (props) => {
       for (const resultado of Object.entries(props.data.resultados)) {
           let dataTime = resultado[0].split("T")
           let dataFormatada = moment(dataTime[0]).format("DD-MM-yy")
-          let data = {"id" : props.data.analisesId[i], "Analista" : props.data.analistas[i],"Data" : `${dataFormatada} - ${dataTime[1]}`, "Resultado" : resultado[1], "unidade" : props.data.unidade,
+          
+          let data = {"id" : props.data.analisesId[i], "Analista" : props.data.analistas[i],"Data" : `${dataFormatada} - ${dataTime[1]}`, "Resultado" : resultado[1].toFixed(2), "unidade" : props.data.unidade,
            "defaultData" : resultado[0], "processoId": props.data.processoId, "etapaId": props.data.etapaId,
            "parametroId": props.data.parametroId }
           resultados.push(data)
@@ -48,14 +49,24 @@ const AnaliseChart = (props) => {
  
   
     
-    setEntries(resultados)
-    },[props.data])
+    setEntries(resultados)},[props.data])
 
-    const handleClick = (event,payload) => {
+      const handleClick = (event,payload) => {
       
       setSelectedAnalise(payload.payload)
       setShow(true)
     }
+
+
+
+
+    const buildDomain = () => {
+        let yMax = props.data.pMax
+        let yMin = props.data.pMin
+        return [yMin,yMax] 
+    }
+
+  
 
     return (
         <>
@@ -65,14 +76,14 @@ const AnaliseChart = (props) => {
         <LineChart  width={props.containerRef.current.offsetWidth} height={250}  
 
             data={entries}
-            margin={{ top: 20, right: 30, left: 0, bottom: 0 }}>
-            <ReferenceLine y={props.data.pMax} stroke="red"  />
-            <ReferenceLine y={props.data.pMaxT} stroke="yellow"  />
-            <ReferenceLine y={props.data.pMinT} stroke="yellow"  />
-            <ReferenceLine y={props.data.pMin} stroke="red"   />
+            margin={{ top: 20, right: 30, left: 30, bottom: 0 }}>
+            <ReferenceLine y={props.data.pMax} label={props.data.pMax}  stroke="red" strokeWidth={2} />
+            <ReferenceLine y={props.data.pMaxT} label={props.data.pMaxT}  stroke="yellow"  strokeWidth={2} />
+            <ReferenceLine y={props.data.pMinT} label={props.data.pMinT} stroke="yellow" strokeWidth={2}  />
+            <ReferenceLine y={props.data.pMin} label={props.data.pMin} stroke="red"  strokeWidth={2} />
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis height={50} tickMargin={20} dataKey="Data" interval="preserveStartEnd" />
-            <YAxis  unit={props.data.unidade}/>
+            <YAxis  unit={props.data.unidade} type={"number"} domain={buildDomain()} tickCount={10}/>
             <Tooltip content={<CustomTooltip ></CustomTooltip>} />
             <Line  type="monotone"  unit={props.data.unidade} dataKey="Resultado" activeDot={{ onClick: handleClick }}  strokeWidth={1.5} stroke="cyan" />
         </LineChart>
