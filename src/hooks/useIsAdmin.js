005a0266811ca39e221ec0f912d11617843c271a
AlexandreNunesDev <74550620+AlexandreNunesDev@users.noreply.github.com>
2023-07-05 16:36:25 -0300

    Update useIsAdmin.js

--- src/hooks/useIsAdmin.js
+++ src/hooks/useIsAdmin.js
@@ -2,8 +2,8 @@ import { useSelector } from "react-redux"
 
 const useIsAdmin = () => {
     const isAdmin =  useSelector(state => state.global.userRole)
-    if(isAdmin === "ADMIN_ROLE") return false
-    return true
+    if(isAdmin === "ADMIN_ROLE") return true
+    return false
 }
 
 export default useIsAdmin
